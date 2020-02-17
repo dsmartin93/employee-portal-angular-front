b7408d966768eae59347c6b0f6c65f01d9387aef
Diego Santaolalla Martin <diegosantaolallamartin@gmail.com>
2020-02-17 01:01:27 +0100

    #3pfpzu Tslint added, initial components created

---
+++ src/app/debug/debug.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DebugRoutingModule } from './debug-routing.module';
+import { DebugComponent } from './components/debug/debug.component';
+
+
+@NgModule({
+  declarations: [
+    DebugComponent
+  ],
+  imports: [
+    CommonModule,
+    DebugRoutingModule,
+  ]
+})
+export class DebugModule { }
