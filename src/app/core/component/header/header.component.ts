594b3cc642de1d0abaaabf418c28e1d2a2272119
Diego Santaolalla Martin <diegosantaolallamartin@gmail.com>
2020-04-05 14:25:47 +0200

    Moved core component from shared to core

---
+++ src/app/core/component/header/header.component.ts
@@ -0,0 +1,40 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { SidebarService } from 'src/app/shared/services/sidebar.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public isAuth: boolean;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private sidebarService: SidebarService
+  ) { }
+
+  public ngOnInit(): void {
+
+    this.authService.getUserAuthorized()
+      .subscribe(
+        (res) => {
+          this.isAuth = res;
+        },
+        (err) => {
+          console.error(err);
+        });
+  }
+
+  public logout(): void {
+    this.authService.deleteAuthData();
+    this.router.navigate(['/login']);
+  }
+
+  public toggleSidenav(): void {
+    this.sidebarService.toggle();
+  }
+}
